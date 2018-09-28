522cf9b443fb8747466099ec272b8fe1b4a9923c
Madhan <madhan.chiluka@gmail.com>
2018-09-28 12:11:19 +0530

    SpringCore_Ex using setter injection

--- SpringIoc/src/main/java/com/spring/core/FountainPen.java
+++ SpringIoc/src/main/java/com/spring/core/FountainPen.java
@@ -4,11 +4,20 @@ public class FountainPen implements Pen{
 	
 	private Ink ink; 
 	
-	FountainPen(Ink ink){
+	/*FountainPen(Ink ink){
 		this.ink = ink;
-	}
+	}*/
+	
 	public void write() {
 		System.out.println("Writing with "+ink.getBrandName()+" of color "+ink.getColor());		
+	}
+
+	public Ink getInk() {
+		return ink;
+	}
+
+	public void setInk(Ink ink) {
+		this.ink = ink;
 	}
 
 }
--- SpringIoc/src/main/java/com/spring/core/Writer.java
+++ SpringIoc/src/main/java/com/spring/core/Writer.java
@@ -3,9 +3,20 @@ package com.spring.core;
 public class Writer {
 	private Pen pen;
 	
-	Writer(Pen pen){
+	
+	public Pen getPen() {
+		return pen;
+	}
+
+
+	public void setPen(Pen pen) {
 		this.pen = pen;
 	}
+
+
+	/*Writer(Pen pen){
+		this.pen = pen;
+	}*/
 	public void write() {
 		pen.write();
 	}
